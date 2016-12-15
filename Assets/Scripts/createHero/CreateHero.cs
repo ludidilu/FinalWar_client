21a2e51eb63201923081e4a91686a58a4ff55f5c
陆地 <87893744@qq.com>
2016-12-15 18:46:25 +0800

    修改英雄生成工具

--- Assets/Scripts/createHero/CreateHero.cs
+++ Assets/Scripts/createHero/CreateHero.cs
@@ -1,32 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.UI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+#if UNITY_EDITOR
+
+using UnityEditor;
+
+#endif
+using System.IO;
 
 public class CreateHero : MonoBehaviour {
 
 	[SerializeField]
-	private int shield;
+	private int shieldValue;
 
 	[SerializeField]
-	private int hp;
+	private int hpValue;
 
 	[SerializeField]
-	private int attack;
+	private int attackkValue;
 
 	[SerializeField]
-	private int shooter;
-
-	[SerializeField]
-	private int supporter;
-
-	[SerializeField]
-	private int defender;
-
-	[SerializeField]
-	private int helper;
-
-	[SerializeField]
-	private int building;
+	private int[] abilitys;
 
 	[SerializeField]
 	private int[] aScore;
@@ -36,6 +34,8 @@ public class CreateHero : MonoBehaviour {
 
 	[SerializeField]
 	private int num;
+
+	private string heroStr = "{0},{1},{2},{3},{4},{5},{6},{7},{8},{9},{10}";
 
 	// Use this for initialization
 	void Start () {
@@ -49,16 +49,130 @@ public class CreateHero : MonoBehaviour {
 
 	public void Click(){
 
-		int id = 1000000;
+		int startId = 1000000;
 
-		for (int i = 0; i < num; i++) {
+		string result = string.Empty;
 
-			for(int m = 0 ; m < 5 ; m++){
+		for(int m = 0 ; m < 5 ; m++){
 
-				int score = aScore[m];
+			for (int i = 0; i < num; i++) {
 
+				int id = startId;
 
+				startId++;
+
+				string str = GetOneHero(id,false,m);
+
+				result = result + str + "\r\n";
+			}
+
+			for (int i = 0; i < num; i++) {
+				
+				int id = startId;
+				
+				startId++;
+				
+				string str = GetOneHero(id,true,m);
+				
+				result = result + str + "\r\n";
 			}
 		}
+
+#if UNITY_EDITOR
+		
+		string path = EditorUtility.SaveFilePanel("a", "a", "", "csv");
+		
+		if (!string.IsNullOrEmpty(path))
+		{
+			FileInfo fi = new FileInfo(path);
+			
+			if (fi.Exists)
+			{
+				fi.Delete();
+			}
+			
+			using (FileStream fs = fi.Create())
+			{
+				using (BinaryWriter bw = new BinaryWriter(fs))
+				{
+					bw.Write(Encoding.UTF8.GetBytes(result));
+				}
+			}
+		}
+		
+#endif
+	}
+
+	private string GetOneHero(int _id,bool _canControl,int _level){
+
+		int id = _id;
+		
+		int score = _canControl ? cScore[_level] : aScore[_level];
+		
+		int ability = (int)(UnityEngine.Random.value * abilitys.Length);
+		
+		int v = abilitys[ability];
+		
+		score -= v;
+		
+		int hp = 1;
+		
+		int shield = 0;
+		
+		int attack = 0;
+		
+		score -= hpValue;
+		
+		while(score > 0){
+			
+			List<int> tmpList = new List<int>();
+			
+			if(score >= shieldValue && shield < 9){
+				
+				tmpList.Add(0);
+			}
+			
+			if(score >= hpValue && hp < 9){
+				
+				tmpList.Add(1);
+			}
+			
+			if(score >= attackkValue && attack < 9){
+				
+				tmpList.Add(2);
+			}
+			
+			if(tmpList.Count == 0){
+				
+				break;
+			}
+			
+			int index = (int)(UnityEngine.Random.value * tmpList.Count);
+			
+			int att = tmpList[index];
+			
+			if(att == 0){
+				
+				shield++;
+				
+				score -= shieldValue;
+				
+			}else if(att == 1){
+				
+				hp++;
+				
+				score -= hpValue;
+				
+			}else{
+				
+				attack++;
+				
+				score -= attackkValue;
+			}
+		}
+
+		string str = string.Format(heroStr,id,string.Empty,_canControl ? "1" : "0",hp,shield,attack,ability,_level + 1,string.Empty,string.Empty,string.Empty);
+
+		return str;
 	}
 }
