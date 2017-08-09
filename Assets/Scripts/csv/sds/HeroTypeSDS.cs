d70fa0576ba62f14193e3782a9213deb67a08d69
Land <ludidilu@163.com>
2017-08-09 22:21:17 +0800

    a

--- Assets/Scripts/csv/sds/HeroTypeSDS.cs
+++ Assets/Scripts/csv/sds/HeroTypeSDS.cs
@@ -1,47 +1,52 @@
 ﻿public partial class HeroTypeSDS : CsvBase, IHeroTypeSDS
 {
-	public bool canDoAction;
+    public bool canDoAction;
 
-	public int attackSpeed;
-	public int defenseSpeed;
-	public int supportSpeed;
+    public int attackSpeed;
+    public int defenseSpeed;
+    public int supportSpeed;
 
-	public int attackTimes;
-	public int thread;
-	public int supportSpeedBonus;
+    public int attackTimes;
+    public int thread;
+    public int supportSpeedBonus;
 
-	public bool GetCanDoAction()
-	{
-		return canDoAction;
-	}
+    public int GetID()
+    {
+        return ID;
+    }
 
-	public int GetAttackSpeed(){
+    public bool GetCanDoAction()
+    {
+        return canDoAction;
+    }
 
-		return attackSpeed;
-	}
+    public int GetAttackSpeed()
+    {
+        return attackSpeed;
+    }
 
-	public int GetDefenseSpeed(){
+    public int GetDefenseSpeed()
+    {
+        return defenseSpeed;
+    }
 
-		return defenseSpeed;
-	}
+    public int GetSupportSpeed()
+    {
+        return supportSpeed;
+    }
 
-	public int GetSupportSpeed(){
+    public int GetAttackTimes()
+    {
+        return attackTimes;
+    }
 
-		return supportSpeed;
-	}
+    public int GetThread()
+    {
+        return thread;
+    }
 
-	public int GetAttackTimes(){
-
-		return attackTimes;
-	}
-
-	public int GetThread()
-	{
-		return thread;
-	}
-
-	public int GetSupportSpeedBonus(){
-
-		return supportSpeedBonus;
-	}
+    public int GetSupportSpeedBonus()
+    {
+        return supportSpeedBonus;
+    }
 }
