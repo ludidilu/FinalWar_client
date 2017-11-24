334f9bf8f9d442b5f50cab8498c6482d18f213f0
ludi <ludidilu@163.com>
2017-11-24 20:11:53 +0800

    a

---
+++ Assets/Scripts/battleManager/ClickText.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using superFunction;
+
+public class ClickText : MonoBehaviour, IPointerClickHandler
+{
+    public const string EVENT_NAME = "clickText";
+
+    [SerializeField]
+    private Text text;
+
+    public void OnPointerClick(PointerEventData _data)
+    {
+        SuperFunction.Instance.DispatchEvent(BattleManager.Instance.gameObject, EVENT_NAME, text.text);
+    }
+}
