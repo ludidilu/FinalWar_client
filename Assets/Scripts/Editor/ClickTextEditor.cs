51ccbff410653e4514b79ca4359574b9ce21a6ca
Land <ludidilu@163.com>
2017-11-24 23:43:39 +0800

    a

---
+++ Assets/Scripts/Editor/ClickTextEditor.cs
@@ -0,0 +1,28 @@
+﻿/**
+ * Author: 1901 (kaixuan1901@gmail.com)
+ * Date: Feb 24, 2017 11:05
+ */
+
+using UnityEditor;
+using UnityEngine;
+
+[CanEditMultipleObjects]
+[CustomEditor(typeof(ClickText))]
+public class ClickTextEditor : UnityEditor.UI.TextEditor
+{
+    private SerializedProperty styleProp;
+
+    protected override void OnEnable()
+    {
+        base.OnEnable();
+        styleProp = serializedObject.FindProperty("clickKey");
+    }
+
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        serializedObject.Update();
+        EditorGUILayout.PropertyField(styleProp, new GUIContent("clickKey"));
+        serializedObject.ApplyModifiedProperties();
+    }
+}
--- Assets/Scripts/battleManager/ClickText.cs
+++ Assets/Scripts/battleManager/ClickText.cs
@@ -1,17 +1,33 @@
-﻿using UnityEngine;
-using UnityEngine.EventSystems;
+﻿using UnityEngine.EventSystems;
 using UnityEngine.UI;
 using superFunction;
+using UnityEngine;
 
-public class ClickText : MonoBehaviour, IPointerClickHandler
+public class ClickText : Text, IPointerClickHandler
 {
+    private static GameObject _eventGo;
+
+    public static GameObject eventGo
+    {
+        get
+        {
+            if (_eventGo == null)
+            {
+                _eventGo = new GameObject("ClickTextEventGo");
+
+                DontDestroyOnLoad(_eventGo);
+            }
+
+            return _eventGo;
+        }
+    }
+
     public const string EVENT_NAME = "clickText";
 
-    [SerializeField]
-    private Text text;
+    public int clickKey;
 
     public void OnPointerClick(PointerEventData _data)
     {
-        SuperFunction.Instance.DispatchEvent(BattleManager.Instance.gameObject, EVENT_NAME, text.text);
+        SuperFunction.Instance.DispatchEvent(eventGo, EVENT_NAME, clickKey);
     }
 }
