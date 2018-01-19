79f7134361d5d93dc75340de661acbb106b21d64
ludi <ludidilu@163.com>
2018-01-19 11:58:51 +0800

    a

--- Assets/Scripts/csv/sds/fix/AuraSDS_c.cs
+++ Assets/Scripts/csv/sds/fix/AuraSDS_c.cs
@@ -2,7 +2,6 @@ using System.IO;
 public class AuraSDS_c {
     public static void Init(AuraSDS _csv, BinaryReader _br){
         _csv.conditionCompare = _br.ReadInt32();
-        _csv.effectData = _br.ReadInt32();
         _csv.effectTarget = _br.ReadInt32();
         _csv.effectTargetNum = _br.ReadInt32();
         _csv.effectType = _br.ReadInt32();
@@ -23,6 +22,11 @@ public class AuraSDS_c {
         _csv.conditionType = new int[lengthconditionType];
         for(int i = 0 ; i < lengthconditionType ; i++){
             _csv.conditionType[i] = _br.ReadInt32();
+        }
+        int lengtheffectData = _br.ReadInt32();
+        _csv.effectData = new int[lengtheffectData];
+        for(int i = 0 ; i < lengtheffectData ; i++){
+            _csv.effectData[i] = _br.ReadInt32();
         }
         _csv.desc = _br.ReadString();
         _csv.eventName = _br.ReadString();
