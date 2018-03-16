84027bd0ad2d335c780016a50428148904a0cd5a
Land <ludidilu@163.com>
2018-03-16 22:19:45 +0800

    a

--- Assets/Scripts/csv/sds/fix/HeroTypeSDS_c.cs
+++ Assets/Scripts/csv/sds/fix/HeroTypeSDS_c.cs
@@ -1,15 +1,16 @@
 using System.IO;
 public class HeroTypeSDS_c {
     public static void Init(HeroTypeSDS _csv, BinaryReader _br){
-        _csv.canAttack = _br.ReadBoolean();
         _csv.attackSpeed = _br.ReadInt32();
         _csv.attackTimes = _br.ReadInt32();
+        _csv.counterTimes = _br.ReadInt32();
         _csv.defenseSpeed = _br.ReadInt32();
         _csv.fearAttackWeight = _br.ReadInt32();
         _csv.fearDefenseWeight = _br.ReadInt32();
         _csv.fearType = _br.ReadInt32();
         _csv.fearValue = _br.ReadInt32();
         _csv.ID = _br.ReadInt32();
+        _csv.recoverShield = _br.ReadInt32();
         _csv.supportSpeed = _br.ReadInt32();
         _csv.thread = _br.ReadInt32();
         _csv.icon = _br.ReadString();
